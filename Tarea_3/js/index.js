6df0a5c92294e8855f291b3210da962f0fb5aff0
Emmanuel <165475266+ItsToji@users.noreply.github.com>
2025-10-24 11:22:34 -0600

    Add files via upload

---
+++ Tarea_3/js/index.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+if (form) {
+    form.addEventListener('submit', function (event) {
+
+        event.preventDefault();
+
+        if (!form.checkValidity()) {
+
+            form.reportValidity();
+            return;
+        }
+
+        Swal.fire({
+            title: 'Reserva realizada',
+            text: 'Estarás recibiendo la reserva por correo electrónico',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+        }).then(() => {
+
+            form.reset();
+        });
+    });
+} else {
+    console.warn('Formulario no encontrado en la página');
+}
+
